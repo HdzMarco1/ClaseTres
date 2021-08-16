04f9a8be1aa96823ba821facee59af6cf8532e01
HdzMarco1 <eh76396@gmail.com>
2021-08-16 15:46:50 -0600

    principio y final

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+         <Image style={{width:100, height:100, margin: "auto", borderRadius: 50}} source={require('./assets/mia.png')}/>
+      <Text style={styles.titulo}>Nombre: Marco Antonio Hernandez Hernandez</Text>
+      <Text style={styles.subtitulo}>Hobbies: Jugar basloncesto, jugar Futbol</Text>
+      <Text style={styles.subtitulo}>Lenguaje de programacion:</Text>
+      <Text style={styles.lenguajes}>Java y Angular</Text>
     </View>
   );
 }
@@ -18,4 +21,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  img: {
+    width:100, 
+    height:100, 
+    margin: "auto", 
+    marginBottom:"30%",
+    marginTop: "0",
+    borderRadius: 50,
+  },
+  titulo: {
+    textAlign: "center",
+    fontSize: 20,
+    fontFamily: "sans-serif", 
+  },
+  subtitulo: {
+    fontSize: 17,
+    fontFamily: "sans-serif",
+    color: "blue"
+  },
+  lenguajes: {
+    fontSize: 15,
+    color: "red"
+  }
 });
